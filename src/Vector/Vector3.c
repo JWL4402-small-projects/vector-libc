ebd52622e13c46dbaa2e2e6baeda6c13a6144d52
JWL <j.lebert4402@gmail.com>
2023-05-27 17:13:49 -0400

    modified functions to remove unnecessary variables

--- src/Vector/Vector3.c
+++ src/Vector/Vector3.c
@@ -3,39 +3,42 @@
 #include "../../include/Vector/vector3.h"
 
 float magnitude_vec3(Vector3 vec3) {
-    float result = sqrt(
+    return sqrt(
         vec3.x * vec3.x + 
         vec3.y * vec3.y + 
-        vec3.z * vec3.z);
-
-    return result;
+        vec3.z * vec3.z
+    );
 }
 
 Vector3 opposite_vec3(Vector3 vec3) {
-    float x = -vec3.x;
-    float y = -vec3.y;
-    float z = -vec3.z;
-
-    Vector3 result = {x, y, z};
+    Vector3 result = {
+        -vec3.x,
+        -vec3.y,
+        -vec3.z
+    };
+    
     return result;
 }
 
 Vector3 normalize_vec3(Vector3 vec3) {
     float scalar = 1 / magnitude_vec3(vec3);
-    float x = vec3.x * scalar;
-    float y = vec3.y * scalar;
-    float z = vec3.z * scalar;
 
-    Vector3 result = {x, y, z};
+    Vector3 result = {
+        vec3.x * scalar,
+        vec3.y * scalar,
+        vec3.z * scalar
+    };
+
     return result;
 }
 
 Vector3 add_vec3(Vector3 vec3_1, Vector3 vec3_2) {
-    float x = vec3_1.x + vec3_2.x;
-    float y = vec3_1.y + vec3_2.y;
-    float z = vec3_1.z + vec3_2.z;
+    Vector3 result = {
+        vec3_1.x + vec3_2.x,
+        vec3_1.y + vec3_2.y,
+        vec3_1.z + vec3_2.z
+    };
 
-    Vector3 result = {x, y, z};
     return result;
 }
 
@@ -57,10 +60,11 @@ float dot_vec3(Vector3 vec3_1, Vector3 vec3_2) {
 }
 
 Vector3 cross_vec3(Vector3 vec3_1, Vector3 vec3_2) {
-    float x = vec3_1.y * vec3_2.z - vec3_1.z * vec3_2.y;
-    float y = vec3_1.z * vec3_2.x - vec3_1.x * vec3_2.z;
-    float z = vec3_1.x * vec3_2.y - vec3_1.y * vec3_2.x;
+    Vector3 result = {
+        vec3_1.y * vec3_2.z - vec3_1.z * vec3_2.y, // x
+        vec3_1.z * vec3_2.x - vec3_1.x * vec3_2.z, // y
+        vec3_1.x * vec3_2.y - vec3_1.y * vec3_2.x  // z
+    };
 
-    Vector3 result = {x, y, z};
     return result;
 }
